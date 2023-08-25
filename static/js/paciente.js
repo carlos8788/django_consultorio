67e1ad80d7efcf780b83b032cee60d902c3d4930
carlos8788 <carlos8788@gmail.com>
2023-08-25 08:21:17 -0300

    js connect new turno, pending api django

--- static/js/paciente.js
+++ static/js/paciente.js
@@ -3,3 +3,21 @@ const modalNuevoTurno = new bootstrap.Modal(document.getElementById('modalNuevoT
 nuevoTurno.addEventListener('click', () => {
     modalNuevoTurno.show();
 });
+
+const confirmarTurno = document.getElementById('confirmarTurno');
+
+confirmarTurno.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const paciente = event.target.querySelector('input[name="paciente"]').value;
+    const user = Object.fromEntries(new FormData(event.target))
+    
+    const SolicitarTurno = {
+        paciente,
+        fecha: user.fecha,
+        hora: user.horario,
+        diagnostico: user.comentario
+    }
+
+    console.log(SolicitarTurno);
+
+})
